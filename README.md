9d7e0bfee511442c26ff8b3636b974a03d0b2105
John Baumert <baumert.john@gmail.com>
2024-07-29 02:31:48 +0000

    Copy CS50 final project to personal repository.

---
+++ README.md
@@ -0,0 +1,78 @@
+# CS50 Final Project - BREW LOG
+
+#### Video Demo: <https://youtu.be/b1xrZoBFZLg>
+
+#### Description
+
+A simple web app that allows the user to create an account and log their
+brewery experience. This project allowed me to demonstrate an array of
+technologies learned including SQL database, API calls, and full-stack
+development.
+
+## Overview
+
+This project uses the typical Python / Flask server structure with "app.py"
+being the main application file. "app.py" has various functions for login /
+logout, brewery search, brewery rating, and confirmation. Most of the
+functions require the user to be logged in utilizing a "login_required"
+decorator as the app itself is intended to be a user log and not just a general
+search. The "templates" folder is typical of a flask project where the
+"base.html" file contains the header and footer code for all the pages and is
+added using jinja to the various other pages.
+
+For data storage, I chose to use an SQL relational database with three tables -
+User, Brewery, Rating.  This allowed me to store username and passwords
+separate of a list of searched breweries. The brewery list is checked when
+adding a rank and existing breweries are linked to it to reduce API calls.  I
+feel this also makes the website perform faster with local data.
+
+## Features
+
+Utilizing CSS the web app is responsive for both mobile and desktop layouts. On
+mobile, I utilized "@media screen" and a custom class to drop certain table
+columns to better fit smaller screens.
+
+## Technologies Used
+
+This is a full-stack project utilizing Bootstrap / HTML for the frontend
+interface and Python / Flask for the backend server.
+
+### Prerequisites
+
+Python 3.12.0 was installed in VS Codespaces at the time of the project.
+
+### Installation
+
+See "requirements.txt" for needed Python libraries.  Recommend that a virtual
+environment is created after which the following command:
+```
+pip install -r requirements.txt
+````
+Once the additional libraries have been installed start the server with the
+command:
+```
+flask run
+```
+
+## Usage
+
+Starting at the home page the user is asked to login or register an account.
+Once an account is created, the user is then able to search for a brewery and
+rate a brewery on beer quality, atmosphere, and location. Afterward, the user
+is shown the overall score and given a chance to re-rate or log the entry.
+
+## Roadmap
+
+- Move away from CS50 library to SQLAlchemy, etc.
+- Enable the user to delete or edit a vote
+- Institute a "user's choice" for highest rated brewery
+- Move to server for hosting to "the world"...
+
+## Acknowledgements
+
+The Open Brewery Database for the free dataset and API -
+<https://www.openbrewerydb.org/>
+
+## Contact
+
+John Baumert - <baumert.john@gmail.com> - <https://www.johnbaumert.com/>
---
+++ app.py
@@ -0,0 +1,203 @@
+import math
+import requests
+
+from cs50 import SQL
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+from functools import wraps
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Constants
+BREWERY_SEARCH = "https://api.openbrewerydb.org/v1/breweries/search"
+
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///user_rating.db")
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    """
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+@app.route("/", methods=["GET", "POST"])
+@login_required
+def index():
+    # Find current breweries and ratings
+    breweries_with_ratings = db.execute(
+        """
+        SELECT
+            b.id AS brewery_id,
+            b.name AS brewery_name,
+            b.city,
+            b.state,
+            r.rating,
+            r.rating_date
+        FROM
+            Brewery b
+        JOIN
+            Rating r ON b.id = r.brewery_id
+        JOIN
+            User u ON r.user_id = u.id
+        WHERE
+            u.id = ?
+        ORDER BY
+            r.rating_date DESC
+    """,
+        session["user_id"],
+    )
+    # for brewery in breweries_with_ratings:
+    #     print(brewery)
+    if request.method == "POST":
+        session["breweries"] = []
+        brewery_search = request.form.get("brewery_search")
+        brewery_search = brewery_search.replace(" ", "_").lower()
+        response = requests.get(BREWERY_SEARCH, params={"query": brewery_search})
+        session["breweries"] = response.json()
+        return render_template("search_results.html", breweries=session["breweries"])
+    return render_template("index.html", breweries=breweries_with_ratings)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+    session.clear()  # Forget any user_id
+    if request.method == "POST":
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM User WHERE username = ?", request.form.get("username")
+        )
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["password_hash"], request.form.get("password")
+        ):
+            return render_template("login.html", error=True)
+        session["user_id"] = rows[0]["id"]  # Remember which user has logged in
+        session["user_name"] = rows[0]["username"]
+        session["breweries"] = []
+        return redirect("/")  # Redirect user to home page
+    return render_template("login.html", error=False)
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+    # Forget any user_id
+    session.clear()
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/rating/<int:brewery_id>", methods=["GET", "POST"])
+@login_required
+def rating(brewery_id):
+    session["brewery"] = session["breweries"][brewery_id]
+    RATING = ["Poor", "Mediocre", "Average", "Good", "Great"]
+    if request.method == "POST":
+        beer_quality = request.form.get("beer_quality")
+        atmosphere = request.form.get("atmosphere")
+        location = request.form.get("location")
+        rating_date = request.form.get("rating_date")
+        user_rating = math.ceil(
+            (
+                round(int(beer_quality) / 25)
+                + round(int(atmosphere) / 25)
+                + round(int(location) / 25)
+            )
+            / 3
+        )  # TODO: Or should this be "floor"?
+        # print(beer_quality, atmosphere, location, RATING[user_rating], rating_date)
+        return render_template(
+            "rating_result.html",
+            brewery=session["brewery"],
+            rating=RATING[user_rating],
+            rating_date=rating_date,
+        )
+    return render_template(
+        "rating.html", brewery=session["brewery"], brewery_id=brewery_id
+    )
+
+
+@app.route("/rating_result", methods=["GET", "POST"])
+@login_required
+def rating_result():
+    if request.method == "POST":
+        rating = request.form.get("rating")
+        rating_date = request.form.get("rating_date")
+        # print(session["brewery"]["name"], rating)
+        # Time to add all this to the database...
+        # Check if brewery is in database and add brewery
+        b = session["brewery"]  # For brevity
+        check = db.execute("SELECT * FROM Brewery where api_id = ?", b["id"])
+        if len(check) == 0:
+            db.execute(
+                "INSERT INTO Brewery (api_id, name, city, state) VALUES (?, ?, ?, ?)",
+                b["id"],
+                b["name"],
+                b["city"],
+                b["state"],
+            )
+        # Get new or existing id of brewery
+        brewery_db_id = db.execute("SELECT * FROM Brewery where api_id = ?", b["id"])
+        # Update Rating table
+        db.execute(
+            "INSERT INTO Rating (brewery_id, rating, user_id, rating_date) VALUES (?, ?, ?, ?)",
+            brewery_db_id[0]["id"],
+            rating,
+            session["user_id"],
+            rating_date,
+        )
+        return redirect("/")
+    return render_template("rating_result.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        # check if user in database and add user to database
+        user = db.execute("SELECT * FROM User where username = ?", username)
+        if len(user) == 0:
+            password_hash = generate_password_hash(password)
+            db.execute(
+                "INSERT INTO User (username, password_hash) VALUES (?, ?)",
+                username,
+                password_hash,
+            )
+            # find user id and login
+            user = db.execute("SELECT * FROM User where username = ?", username)
+            session["user_id"] = user[0]["id"]
+            return redirect("/")
+        else:
+            return render_template("register.html", error=True)
+    return render_template("register.html", error=False)
+
+
+@app.route("/search_results", methods=["GET", "POST"])
+@login_required
+def search_results():
+    if request.method == "POST":
+        return render_template("search_results.html")
+    return redirect("/")
