7ab1abec783e6054a95bc2f73f459767961f1c4e
John Baumert <baumert.john@gmail.com>
2024-07-30 03:13:16 +0000

    Add back missing images. Adjust for GCP testing.

--- app.py
+++ app.py
@@ -203,3 +203,8 @@ def search_results():
     if request.method == "POST":
         return render_template("search_results.html")
     return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
+
